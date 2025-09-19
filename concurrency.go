123599ba8452b387f168b4341bd62e4ee7c79ab1
iamsaurav-karki <sauravkarki102@gmail.com>
2025-09-19 17:31:04 +0545

    added concurrency,error handling

---
+++ concurrency.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func sayHello() {
+    fmt.Println("Hello from goroutine")
+}
+
+func main() {
+    go sayHello() // run concurrently
+    time.Sleep(1 * time.Second) // wait for goroutine
+    fmt.Println("Main function ends")
+}
+
